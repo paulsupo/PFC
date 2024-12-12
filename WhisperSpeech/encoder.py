c7d8db92989d55ce225c1b4402a0d799a69c6159
paulsupo <42578857+paulsupo@users.noreply.github.com>
2024-12-12 10:56:19 -0500

    Delete WhisperSpeech/encoder.py

--- WhisperSpeech/encoder.py
+++
@@ -1,6 +0,0 @@
-import numpy as np
-
-class AcousticEncoder:
-    def encode(self, semantic_tokens):
-        # Se codifica los tokens semánticos en tokens acústicos
-        return np.tanh(semantic_tokens)
